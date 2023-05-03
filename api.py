e1c361d6bee247ea00e0478d7bd144b628c9e4c2
zhanbao2000 <zhanbao2000@gmail.com>
2023-05-03 15:41:29 +0800

    add `**kwargs` pass in `parse_url_as` function

--- api.py
+++ api.py
@@ -5,26 +5,26 @@ from .utils import parse_url_as
 __all__ = ['get_songs_all', 'get_song', 'get_bands_all', 'get_event_track', 'get_events_all']
 
 
-async def get_songs_all() -> SongsAll:
+async def get_songs_all(**kwargs) -> SongsAll:
     """Get all songs from bestdori"""
-    return await parse_url_as('https://bestdori.com/api/songs/all.7.json', SongsAll)
+    return await parse_url_as('https://bestdori.com/api/songs/all.7.json', SongsAll, **kwargs)
 
 
-async def get_song(song_id: int) -> Song:
+async def get_song(song_id: int, **kwargs) -> Song:
     """Get a song from bestdori"""
-    return await parse_url_as(f'https://bestdori.com/api/songs/{song_id}.json', Song)
+    return await parse_url_as(f'https://bestdori.com/api/songs/{song_id}.json', Song, **kwargs)
 
 
-async def get_bands_all() -> BandsAll:
+async def get_bands_all(**kwargs) -> BandsAll:
     """Get all bands (name only) from bestdori"""
-    return await parse_url_as('https://bestdori.com/api/bands/all.1.json', BandsAll)
+    return await parse_url_as('https://bestdori.com/api/bands/all.1.json', BandsAll, **kwargs)
 
 
-async def get_event_track(server: LanguageLike, event: int, tier: LiteralTier) -> EventTrack:
+async def get_event_track(server: LanguageLike, event: int, tier: LiteralTier, **kwargs) -> EventTrack:
     """Get event track from bestdori"""
-    return await parse_url_as(f'https://bestdori.com/api/tracker/data?server={server}&event={event}&tier={tier}', EventTrack)
+    return await parse_url_as(f'https://bestdori.com/api/tracker/data?server={server}&event={event}&tier={tier}', EventTrack, **kwargs)
 
 
-async def get_events_all() -> EventsAll:
+async def get_events_all(**kwargs) -> EventsAll:
     """Get all events from bestdori"""
-    return await parse_url_as('https://bestdori.com/api/events/all.6.json', EventsAll)
+    return await parse_url_as('https://bestdori.com/api/events/all.6.json', EventsAll, **kwargs)
